fb986f5763c1a741dfa0622af657af2e14646373
Kunall <146503735+Kunals0612@users.noreply.github.com>
2023-12-13 10:55:40 +0530

    Update Assignment 9.cpp

--- Assignment 9.cpp
+++ Assignment 9.cpp
@@ -42,27 +42,103 @@ void selectionSort(T arr[], int n)
 
 int main()
 {
-    int n;
-    cout << "Enter the size of array: " << endl;
-    cin >> n;
-    float a[n];
-    cout << "Enter the array elements: " << endl;
-    for (int i = 0; i < n; i++)
+    cout << "1. sort for float values " << endl;
+    cout << "2. sort for int values " << endl;
+    cout << "3. sort for char values " <<endl;
+    cout << "4. exit "<<endl;
+    while (true)
     {
-        cin >> a[i];
-    }
-    selectionSort<float>(a, n);
-    cout << "selection sort" << endl;
-    for (int i = 0; i < n; i++)
-    {
-        cout << a[i] << " ";
-    }
-    cout << endl;
-    insertionSort<float>(a, n);
-    cout << "Insertion sort" << endl;
-    for (int i = 0; i < n; i++)
-    {
-        cout << a[i] << " ";
+        int choice;
+        cout << "Enter the choice: " << endl;
+        cin >> choice;
+        if (choice == 1)
+        {
+            int n;
+            cout << "Enter the size of array: " << endl;
+            cin >> n;
+            float a[n];
+            cout << "Enter the array elements: " << endl;
+            for (int i = 0; i < n; i++)
+            {
+                cin >> a[i];
+            }
+            selectionSort<float>(a, n);
+            cout << "selection sort" << endl;
+            for (int i = 0; i < n; i++)
+            {
+                cout << a[i] << " ";
+            }
+            cout << endl;
+            insertionSort<float>(a, n);
+
+            cout << "Insertion sort" << endl;
+            for (int i = 0; i < n; i++)
+            {
+                cout << a[i] << " ";
+            }
+            cout << endl;
+        }
+        else if (choice == 2)
+        {
+            int n;
+            cout<<"Enter the size of the array: "<<endl;
+            cin>>n;
+            int b[n];
+            cout << "Enter the array elements: " << endl;
+            for (int i = 0; i < n; i++)
+            {
+                cin >> b[i];
+            }
+            selectionSort<int>(b, n);
+            cout << "selection sort" << endl;
+            for (int i = 0; i < n; i++)
+            {
+                cout << b[i] << " ";
+            }
+            cout << endl;
+            insertionSort<int>(b, n);
+            cout << "Insertion sort" << endl;
+            for (int i = 0; i < n; i++)
+            {
+                cout << b[i] << " ";
+            }
+            cout<<endl;
+        }
+        else if(choice == 3)
+        {
+            
+            int n;
+            cout<<"Enter the size of the array: "<<endl;
+            cin>>n;
+            char c[n];
+            cout << "Enter the array elements: " << endl;
+            for (int i = 0; i < n; i++)
+            {
+                cin >> c[i];
+            }
+            selectionSort<char>(c, n);
+            cout << "selection sort" << endl;
+            for (int i = 0; i < n; i++)
+            {
+                cout << c[i] << " ";
+            }
+            cout << endl;
+            insertionSort<char>(c, n);
+            cout << "Insertion sort" << endl;
+            for (int i = 0; i < n; i++)
+            {
+                cout << c[i] << " ";
+            }
+            cout<<endl;
+        }
+        else if(choice == 4)
+        {
+            break;
+        }
+        else
+        {
+            cout<<"Invalid Input"<<endl;
+        }
     }
     return 0;
 }
